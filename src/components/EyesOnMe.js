50d08aabb18337c1137d9d250f490a7c853731e0
Isaac Wilhite <isaacwilhite987@gmail.com>
2023-10-03 22:25:03 -0600

    solution

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+const EyesOnMe = () => {
+    const focused = () => {
+        console.log("Good!")
+    }
+    const notFocused = () => {
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <button onFocus={focused} onBlur={notFocused}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
